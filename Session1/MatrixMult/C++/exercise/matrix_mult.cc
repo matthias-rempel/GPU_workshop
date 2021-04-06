b1fd105d2618b38a32f93a78a280d296eb7ff53b
pilotchute <akootz@ucar.edu>
2021-04-06 21:41:49 +0000

    first changes

--- Session1/MatrixMult/C++/exercise/matrix_mult.cc
+++ Session1/MatrixMult/C++/exercise/matrix_mult.cc
@@ -5,12 +5,16 @@
 
 void openacc_matrix_mult(const float *A,const float *B, float *C, const int rowsA, const int colsB, const int rowsB)
 {
-  
+ 
   float temp = 0.0;
+  #pragma acc data copyin(A[0:rowsA*rowsB],B[0:rowsB*colsB]) copyout(C[0:rowsA*colsB])
+  {
+  #pragma acc parallel loop collapse(2) reduction(+:temp)	  
   for (int i = 0; i < rowsA; i++)
     for (int j = 0; j < colsB; j++)
     {
         temp = 0.0;
+        #pragma acc loop reduction(+:temp)
         for (int k = 0; k < rowsB; k++)
         {
             temp += A[i*rowsB+k] * B[k*colsB+j];
@@ -18,6 +22,7 @@ void openacc_matrix_mult(const float *A,const float *B, float *C, const int rows
 
         C[i*colsB+j] = temp;
    }
+  }
 }
 
 
